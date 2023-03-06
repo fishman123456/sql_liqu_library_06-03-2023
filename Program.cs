945b99418bad1e9172b081b6dc057b06f36b16e0
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-06 20:50:19 +0700

    добавить обьекты в базу данных

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿
-using Azure;
+
+using System;
+using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using sql_liqu_library_06_03_2023;
 using System.Data;
@@ -50,14 +52,36 @@ using (SqlConnection connection = new SqlConnection(connectionString))
     // добавляем два объекта
     string sqlExpression3 = "INSERT INTO Books (Title, PRICE, PAGES) VALUES ('Alice', 32, 5200), ('Bob', 28, 3258)";
 
-    using (SqlConnection connection = new SqlConnection(connectionString))
+using (SqlConnection connection = new SqlConnection(connectionString3))
+{
+
+    //Console.WriteLine(sqlExpression + "\n");
+    connection.Open();
+    SqlCommand command = new SqlCommand(sqlExpression, connection);
+    SqlDataReader reader = command.ExecuteReader();
+    book one = new book();
+    List<book> two = new List<book>();
+    if (reader.HasRows) // если есть данные
     {
-        await connection.OpenAsync();
-
-        SqlCommand command = new SqlCommand(sqlExpression, connection);
-        int number = await command.ExecuteNonQueryAsync();
-        Console.WriteLine($"Добавлено объектов: {number}");
+        // выводим названия столбцов
+        // Console.WriteLine("{0}\t{1}\t{2}", reader.GetName(0),  reader.GetName(1), reader.GetName(2));
+        int line = 0;
+        while (reader.Read()) // построчно считываем данные
+        {
+            //if (line == 0)
+            //{
+            //    line++;
+            //}
+            //two.Add(new book());
+            one.Title = reader.GetString(0);
+            one.Price = reader.GetInt32(1);
+            one.Pages = reader.GetInt32(2);
+           // Console.WriteLine($"Имя: {one.Title}  цена: {one.Price}  страниц: {one.Pages}");
+        }
     }
-    Console.Read();
+    // передаём обьект в sql
 
-//}
+    //  Console.WriteLine($"Имя: {one.Title}  цена: {one.Price}  страниц: {one.Pages}");
+    reader.Close();
+}
+
