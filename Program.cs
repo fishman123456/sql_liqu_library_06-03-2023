41f9a4ca3807e5374b41ec57f4aee821102397e0
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-06 19:04:24 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=True";
+
+string sqlExpression = "SELECT * FROM Authors";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    Console.WriteLine(sqlExpression + "\n");
+    connection.Open();
+    SqlCommand command = new SqlCommand(sqlExpression, connection);
+    SqlDataReader reader = command.ExecuteReader();
+
+    if (reader.HasRows) // если есть данные
+    {
+        // выводим названия столбцов
+        Console.WriteLine("{0}\t{1}\t{2}", reader.GetName(0), reader.GetName(1), reader.GetName(2));
+
+        while (reader.Read()) // построчно считываем данные
+        {
+            object id = reader.GetValue(0);
+            object name = reader.GetValue(1);
+            object age = reader.GetValue(2);
+
+            Console.WriteLine("{0} \t{1} \t{2}", id, name, age);
+        }
+    }
+
+    reader.Close();
+}
+
+Console.Read();
---
+++ book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql_liqu_library_06_03_2023
+{
+    public class book
+    {
+
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public int Pages { get; set; }
+
+        public book() { }
+        public book(string _Title, int _Price, int _Pages)
+        {
+            Title = _Title;
+            Price = _Price;
+            Pages = _Pages;
+        }
+    }
+}
